7cecd83b720ec3f853b29d9301707c7ce42f6ce7
Fagner Sales <fs.fagnersales@gmail.com>
2021-02-21 03:02:05 -0300

    add apple error

--- tests/error.test.ts
+++ tests/error.test.ts
@@ -5,6 +5,13 @@ class AndroidError extends Error {
   }
 }
 
+class AppleError extends Error {
+  public name: string = 'AppleCompilerError';
+  constructor(public message: string) {
+    super();
+  }
+}
+
 function compileAndroidCode() {
   throw new AndroidError('you are using the wrong JDK');
 }
@@ -13,6 +20,7 @@ test('compiling android goes as expected', () => {
   expect(() => compileAndroidCode()).toThrow();
   expect(() => compileAndroidCode()).toThrow(Error);
   expect(() => compileAndroidCode()).toThrow(AndroidError);
+  expect(() => compileAndroidCode()).not.toThrow(AppleError);
 
   expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
   expect(() => compileAndroidCode()).toThrow(/JDK/);
